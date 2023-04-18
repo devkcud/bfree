a29ec574f90e013fc91618632c89d2c464617210
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-04-18 15:36:00 -0300

    feat(help): Add help flag (-h)

--- src/utils.rs
+++ src/utils.rs
@@ -1,2 +1,5 @@
 #[allow(dead_code)]
 pub mod args;
+
+#[allow(dead_code)]
+pub mod help;
---
+++ src/utils/help.rs
@@ -0,0 +1,55 @@
+use std::{env::args, process::exit};
+use prettytable::{Table, format, row};
+
+pub struct HelpMenu {
+    project_name: String,
+    project_description: String,
+
+    commands: Vec<(char, String)>,
+    flags: Vec<(char, String)>,
+}
+
+impl HelpMenu {
+    pub fn new(project_description: &str) -> Self {
+        return HelpMenu {
+            // May God forgive my sins.
+            project_name: String::from(*args().collect::<Vec<String>>().first().unwrap_or(&"bfree".to_string()).to_string().split('/').collect::<Vec<&str>>().last().unwrap_or(&"bfree")),
+            project_description: String::from(project_description),
+
+            commands: Vec::new(),
+            flags: Vec::new(),
+        };
+    }
+
+    pub fn add_command(&mut self, id: char, description: &str) -> &mut Self {
+        self.commands.push((id, String::from(description)));
+        return self;
+    }
+
+    pub fn add_flag(&mut self, id: char, description: &str) -> &mut Self {
+        self.flags.push((id, String::from(description)));
+        return self;
+    }
+
+    pub fn assemble(&self) {
+        let mut command_table = Table::new();
+        command_table.set_format(*format::consts::FORMAT_BOX_CHARS);
+        command_table.set_titles(row!["Command", "Description"]);
+
+        let mut flag_table = Table::new();
+        flag_table.set_format(*format::consts::FORMAT_BOX_CHARS);
+        flag_table.set_titles(row!["Flag   ", "Description"]);
+
+        for (command, description) in &self.commands { command_table.add_row(row![command, description]); }
+        for (flag, description) in &self.commands {
+            let mut flag = flag.to_string();
+            flag.insert(0, '-');
+
+            flag_table.add_row(row![flag.to_string(), description]);
+        }
+
+        command_table.printstd();
+        flag_table.printstd();
+        exit(0);
+    }
+}
